50a7bc9ac6ae534847c9ca4e5a532e252fc6fde7
Dimitris <jimelectrician@gmail.com>
2022-07-03 13:10:39 +0300

    initial commit

---
+++ src/components/Header.vue
@@ -0,0 +1,34 @@
+<template>
+    <header>
+        <h1>{{ title }}</h1>
+        <Button @btn-click="$emit('toggle-add-task')" :text="showAddTask ? 'Close' : 'Add Task'"
+            :color="showAddTask ? 'red' : 'green'" />
+    </header>
+</template>
+
+<script>
+import Button from '../components/Button';
+
+export default {
+    // eslint-disable-next-line vue/multi-word-component-names
+    name: 'Header',
+    props: {
+        title: String,
+        color: String,
+        showAddTask: Boolean,
+    },
+    components: {
+        Button
+    }
+}
+</script>
+
+<style scoped>
+header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: 20px;
+}
+</style>
+
---
+++ src/components/Tasks.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <div v-for="task in tasks" :key="task.id">
+            <TaskItem :task="task" @delete-task="$emit('delete-task', task.id)"
+                @toggle-reminder="$emit('toggle-reminder', task.id)" />
+        </div>
+    </div>
+</template>
+
+<script>
+import TaskItem from './TaskItem'
+
+export default {
+    // eslint-disable-next-line vue/multi-word-component-names
+    name: 'Tasks',
+    props: {
+        tasks: Array
+    },
+    components: {
+        TaskItem,
+    },
+    emits: ['delete-task', 'toggle-reminder'],
+}
+</script>
